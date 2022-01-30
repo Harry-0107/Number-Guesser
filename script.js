526e9b2de3bffbf5b36ba3bb086882bbf285cc24
harish vishwa <77568172+Harry-0107@users.noreply.github.com>
2022-01-30 14:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+let number = Math.trunc(Math.random()*20) + 1
+let score = 20;
+let highscore = 0;
+
+//check button
+document.querySelector('.check').addEventListener('click' , function() {
+    const guess = Number(document.querySelector('.guess').value);
+    
+    //no input
+    if (!guess) { 
+        document.querySelector('.message').textContent = '🤔 NO INPUT PROVIDED';
+    }
+    
+    //correct guess
+    else if (guess === number) 
+    {
+        document.querySelector('.message').textContent = '😎 YOU GUESSED IT RIGHT. GG!!';
+
+        //winning color ,number reveal, size change
+        document.querySelector('.number').textContent = number;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        //highscore
+        if (score>highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = score;
+
+        }
+    }
+
+
+    //lower guess
+    else if (guess < number)
+    {
+        if (score >1) 
+        {
+            document.querySelector('.message').textContent = '👇🏼You Guessed a Lower Number';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else 
+        {
+            document.querySelector('.message').textContent = '💀 YOU lOST THE GAME';
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('body').style.backgroundColor = '#ff6666'
+        }
+
+        
+    }
+    //higher guess
+    else if (guess > number) 
+    {
+        if (score>1)
+        {
+        document.querySelector('.message').textContent = '👆🏼You Guessed a Higher Number'
+        score--;
+        document.querySelector('.score').textContent = score;
+        }
+        else 
+        {
+            document.querySelector('.message').textContent = '💀 YOU lOST THE GAME';
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('body').style.backgroundColor = '#ff6666'
+        }
+    }
+});
+
+
+//Again button
+document.querySelector('.again').addEventListener('click', function() {
+
+    //main body restoration
+    score = 20;
+    number = Math.trunc(Math.random()*20) + 1;
+    document.querySelector('.message').textContent = '💭 Start guessing...';
+    document.querySelector('.score').textContent = score; //the input is in textContent format
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+
+    //css restoration
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    
+});
+
+
